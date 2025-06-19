109427f452e1396b941cef472f9d093a1f08dda7
Dame <gban1mj27@gmail.com>
2025-06-19 15:47:39 +0700

    fixed typo font poppins

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -20,10 +21,10 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         leading: Container(
           margin: EdgeInsets.all(10),
-          // width: 30,
-          // height: 30,
+          width: 20,
+          height: 20,
           decoration: BoxDecoration(
-            color: Colors.black,
+            color: Color(0xffF7F8F8),
             borderRadius: BorderRadius.circular(10),
           ),
         ),
