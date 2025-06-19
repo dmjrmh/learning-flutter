45ec7ead8893148e859673f6743d7d25864f61dc
Dame <gban1mj27@gmail.com>
2025-06-19 15:57:50 +0700

    added appBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,20 +19,42 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0.0,
         centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          alignment: Alignment.center,
-          width: 37,
-          child: SvgPicture.asset(
-            'assets/icons/Arrow - Left 2.svg',
-            width: 5,
-            height: 5,
-          ),
-          decoration: BoxDecoration(
-            color: Color(0xffF7F8F8),
-            borderRadius: BorderRadius.circular(10),
+        leading: GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            child: SvgPicture.asset(
+              'assets/icons/Arrow - Left 2.svg',
+              width: 20,
+              height: 20,
+            ),
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
         ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              width: 37,
+              child: SvgPicture.asset(
+                'assets/icons/dots.svg',
+                width: 5,
+                height: 5,
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
